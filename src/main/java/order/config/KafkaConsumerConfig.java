9952294eed029d1048d674470a3643c8d453676d
Danilo Meneghel <danilo.meneghel@gmail.com>
2025-03-29 10:11:59 -0300

    Atualização de arquivos

--- src/main/java/order/config/KafkaConsumerConfig.java
+++ src/main/java/order/config/KafkaConsumerConfig.java
@@ -28,6 +28,7 @@ public class KafkaConsumerConfig {
         props.put(ConsumerConfig.GROUP_ID_CONFIG, "group-id");
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
+        props.put(JsonDeserializer.TRUSTED_PACKAGES, "order.dto"); 
         return new DefaultKafkaConsumerFactory<>(props, new StringDeserializer(), new JsonDeserializer<>(OrderCreateDTO.class));
     }
 
--- src/main/java/order/config/ZookeeperConfig.java
+++ src/main/java/order/config/ZookeeperConfig.java
@@ -3,17 +3,24 @@ package order.config;
 import org.apache.curator.framework.CuratorFramework;
 import org.apache.curator.framework.CuratorFrameworkFactory;
 import org.apache.curator.retry.ExponentialBackoffRetry;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ZookeeperConfig {
 
+    @Value("${zookeeper.service.host}")
+    private String serviceHost;
+
+    @Value("${zookeeper.service.port}")
+    private int servicePort;
+
     @Bean
     public CuratorFramework curatorFramework() {
         CuratorFramework client = CuratorFrameworkFactory.newClient(
-                "localhost:2181", 
-                new ExponentialBackoffRetry(1000, 3)
+            String.format("%s:%d", serviceHost, servicePort), 
+            new ExponentialBackoffRetry(1000, 3)
         );
         client.start();
         return client;
